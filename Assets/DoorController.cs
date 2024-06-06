7f76f920316227fabeef4bea3f7e3746a21d57f5
lom3e <147386486+lom3e@users.noreply.github.com>
2024-06-06 22:37:52 +0200

    Added Torch, Axe, Drop, Pick Up and Door Controls.

--- Assets/DoorController.cs
+++ Assets/DoorController.cs
@@ -1,35 +1,87 @@
+using System;
+using System.Collections;
 using UnityEngine;
 
 public class DoorController : MonoBehaviour
 {
-    public float openAngle = 90f; // Angolo di apertura
-    public float closeAngle = 0f; // Angolo di chiusura
-    public float smooth = 2f; // Velocità di apertura/chiusura
-    private bool isOpen = false; // Stato della porta
-    private Quaternion targetRotation; // Rotazione di destinazione
+    public GameObject Door;
+    public Boolean IsDoorOpened;
+    public GameObject OpenText;
+    public GameObject CloseText;
+    public float rotationSpeed = 2.0f; // Velocità di rotazione
+
+    private bool isRotating = false; // Flag per controllare se la porta sta ruotando
+    private Quaternion closedRotation;
+    private Quaternion openedRotation;
 
     void Start()
     {
-        targetRotation = transform.rotation;
+        OpenText.SetActive(false);
+        CloseText.SetActive(false);
+
+        // Imposta le rotazioni chiuse e aperte
+        closedRotation = Door.transform.rotation;
+        openedRotation = Quaternion.Euler(Door.transform.eulerAngles + new Vector3(0, 90, 0));
     }
 
-    void Update()
+    private void OnTriggerEnter(Collider other)
     {
-        // Lerp per una transizione graduale
-        transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, Time.deltaTime * smooth);
+        if (other.gameObject.tag == "Player")
+        {
+            if (!IsDoorOpened)
+            {
+                OpenText.SetActive(true);
+            }
+            else
+            {
+                CloseText.SetActive(true);
+            }
+        }
     }
 
-    public void ToggleDoor()
+    private void OnTriggerExit(Collider other)
     {
-        // Alterna tra aperto e chiuso
-        isOpen = !isOpen;
-        if (isOpen)
+        if (other.gameObject.tag == "Player")
         {
-            targetRotation = Quaternion.Euler(transform.eulerAngles.x, 90f, transform.eulerAngles.z);
+            OpenText.SetActive(false);
+            CloseText.SetActive(false);
         }
-        else
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.tag == "Player" && !isRotating)
         {
-            targetRotation = Quaternion.Euler(transform.eulerAngles.x, closeAngle, transform.eulerAngles.z);
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                if (!IsDoorOpened)
+                {
+                    StartCoroutine(RotateDoor(openedRotation)); // Ruota alla posizione aperta
+                    IsDoorOpened = true;
+                    OpenText.SetActive(false);
+                    CloseText.SetActive(true);
+                }
+                else
+                {
+                    StartCoroutine(RotateDoor(closedRotation)); // Ruota alla posizione chiusa
+                    IsDoorOpened = false;
+                    OpenText.SetActive(true);
+                    CloseText.SetActive(false);
+                }
+            }
         }
+    }
+
+    private IEnumerator RotateDoor(Quaternion toRotation)
+    {
+        isRotating = true; // Imposta la flag per indicare che la porta sta ruotando
+        Quaternion fromRotation = Door.transform.rotation;
+        for (float t = 0; t < 1; t += Time.deltaTime * rotationSpeed)
+        {
+            Door.transform.rotation = Quaternion.Slerp(fromRotation, toRotation, t);
+            yield return null;
+        }
+        Door.transform.rotation = toRotation; // Assicurati che la rotazione finisca esattamente all'angolo desiderato
+        isRotating = false; // Reimposta la flag una volta completata la rotazione
     }
 }
